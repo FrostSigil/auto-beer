9cdc27f204323b8db8c726edbc56e2a089e8cae5
orynes <a.mochanry2468@gmail.com>
2024-01-20 20:22:28 +0200

    fix

--- index.js
+++ index.js
@@ -1,51 +1,68 @@
 module.exports = function AutoBeer(mod) {
 
-	mod.game.initialize(["me.abnormalities"]);
+	mod.game.initialize(["me.abnormalities", "inventory"]);
 	const { player } = mod.require.library;
-	const beer = 80081;
-	const bustAbn = {
-		warrior: [ 100800, 100801 ], //Смертельная игра (Воин)
-		lancer: [ 201903, 201904 ], // Адреналин (Рыцарь)
-		slayer: [ 300800, 300805 ], // Холодная кровь (Убийца)
-		berserker: [ 401705 ], // Бешенство (Берсерк)
-		sorcerer: [ 503061 ], // Мана буст (Маг)
-		archer: [ 602221 ], // Мелодия (Лучник)
-		priest: [ 805800 ], // Крылья (Священник)
-		elementalist: [ 702004 ], // Разрушитель (Мистик)
-		soulless: [ 10151010 ], // Жатва (Жнец)
-		engineer: [ 10152340 ], // Джет панель (Инженер)
-		fighter: [ 10153210 ], // Ураганная серия (Крушитель)
-		assassin: [ 10154480 ], // Гармония (Шиноби)	
-		valkyrie: [ 10155130, 10155512 ] // Багровка / Последняя песня(Валькирия)
-	};
-    let enable = true;
 
-	mod.command.add('beer', {
+	mod.command.add("beer", {
 		$default() {
-			enable = !enable;
-			mod.command.message(`${enable ? 'en' : 'dis'}abled`);
-		}
+			mod.settings.enabled = !mod.settings.enabled;
+			mod.command.message(`Автоматическое использование пива: ${mod.settings.enabled ? "Вкл" : "Выкл"}ючено`);
+		},
+		abnormal() {
+			mod.settings.abnormal = !mod.settings.abnormal
+			if (mod.settings.skill || mod.settings.skill_inst) {mod.settings.skill = false; mod.settings.skill_inst = false}
+			mod.command.message(`Автопиво по абнормали: ${mod.settings.abnormal ? "Вкл" : "Выкл"}ючено`);
+		},
+		skill(arg) {
+			mod.settings.skill = !mod.settings.skill
+			if (arg !== null) {arg = mod.settings.skill_id;}
+			if (mod.settings.abnormal || mod.settings.skill_inst) {mod.settings.abnormal = false; mod.settings.skill_inst = false}
+			mod.command.message(`Автопиво по скиллу: ${mod.settings.skill ? "Вкл" : "Выкл"}ючено`);
+		},
+		skillinst(arg) {
+			mod.settings.skill_inst = !mod.settings.skill_inst;
+			if (arg !== null) {arg = mod.settings.skill_id;}
+			if (mod.settings.abnormal || mod.settings.skill) {mod.settings.abnormal = false; mod.settings.skill = false}
+			mod.command.message(`Автопиво по инстант скиллу: ${mod.settings.skill_inst ? "Вкл" : "Выкл"}ючено`);
+		},
 	});
 	
-	mod.hook('S_ABNORMALITY_BEGIN', "*", event => { useBeer(event); });
+	mod.hook("S_ABNORMALITY_BEGIN", "*", { order: Infinity, filter: { fake: null } }, event => {
+		if (!mod.game.me.is(event.target) || !mod.settings.abnormal) return;
+		if (mod.settings.burst_abnormal[mod.game.me.class] && mod.settings.burst_abnormal[mod.game.me.class].includes(event.id)) {
+			useBeer();
+		}
+	});
 
-	function useBeer(event) {
-		if (!mod.game.me.is(event.target) || !mod.game.me.alive) return;
-		if (bustAbn[mod.game.me.class] && bustAbn[mod.game.me.class].includes(event.id)) {
-			useItem(beer);
+	mod.hook("C_START_SKILL", "*", { order: Infinity, filter: { fake: null } }, event => {
+		if (event.skill.id !== mod.settings.skill_id || !mod.settings.skill) return;
+		useBeer();
+	});
+
+	mod.hook("C_START_INSTANCE_SKILL", "*", { order: Infinity, filter: { fake: null } }, event => {
+		if (event.skill.id !== mod.settings.skill_id || !mod.settings.skill_inst) return;
+		useBeer();
+	});
+
+	function useBeer() {
+		if (!mod.game.me.alive) return;
+		const item = mod.settings.item;
+		const beer	= mod.game.inventory.bagItems.find(x => item.includes(x.id));
+		if (beer && mod.game.inventory.findAllInBagOrPockets(beer.id).length !== 0) {
+			useItem(beer.id, beer.dbid);
 		}
 	}
 
-	function useItem(id) {
-		if (!player)
-			return;
+	function useItem(id, dbid) {
+		if (!player) return;
 		mod.send("C_USE_ITEM", "*", {
 			gameId: mod.game.me.gameId,
-			id: id,
+			id,
+			dbid,
 			amount: 1,
 			loc: player.loc,
 			w: player.loc.w,
 			unk4: true
 		});
 	}
-}
+};
